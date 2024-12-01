b30481b8490ea25be7cb486e89d728ca44fa4f57
Kien Nguyen <knguyen2@macalester.edu>
2024-11-30 22:13:41 -0600

    Create FunctionNode.java

---
+++ src/wordy/ast/FunctionNode.java
@@ -0,0 +1,42 @@
+package wordy.ast;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import wordy.interpreter.EvaluationContext;
+
+public class FunctionNode extends StatementNode {
+
+    public FunctionNode() {
+        
+    }    
+
+    @Override
+    protected void doRun(EvaluationContext context) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doRun'");
+    }
+
+    @Override
+    public Map<String, ASTNode> getChildren() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getChildren'");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'equals'");
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hashCode'");
+    }
+    
+}
